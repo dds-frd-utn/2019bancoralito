ab4eacd87f870a05b4522c3fef74f17aa3165b39
brios <brios@itport.com.ar>
2019-07-13 19:37:57 -0300

    verSession devuelve json

--- src/main/java/utn/frd/grupo_tbt/servlet/VerSession.java
+++ src/main/java/utn/frd/grupo_tbt/servlet/VerSession.java
@@ -12,6 +12,8 @@ package utn.frd.grupo_tbt.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,6 +21,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @WebServlet("/VerSession")
 public class VerSession extends HttpServlet {
@@ -27,10 +31,23 @@ public class VerSession extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession misession= (HttpSession) request.getSession();
 
-        String usuario= (String) misession.getAttribute("usuario");
-
+        JSONObject usuario = new JSONObject();
+        
+        try {
+            usuario.put("idCliente", Integer.parseInt((String)misession.getAttribute("idCliente")))
+                    .put("idTipoCliente",Integer.parseInt((String)misession.getAttribute("idTipoCliente")))
+                    .put("usuario",(String)misession.getAttribute("usuario"))
+                    .put("contrasenia",(String)misession.getAttribute("contrasenia"))
+                    .put("nombre",(String)misession.getAttribute("nombre"))
+                    .put("apellido",(String)misession.getAttribute("apellido"))
+                    .put("du",Integer.parseInt((String)misession.getAttribute("du")))
+                    ;
+        } catch (JSONException ex) {
+            Logger.getLogger(VerSession.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         PrintWriter pw= response.getWriter();
-        pw.println("<html><body>"+ usuario);
+        pw.println(usuario.toString());
         pw.close();
     }
 }
